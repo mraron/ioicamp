c8d5790dc7bc7d209fc291ea06bcd23d294486f3
Noszály Áron <noszalyaron4@gmail.com>
2018-06-21 12:05:23 +0200

    progress

--- tapasztalat/main.cpp
+++ tapasztalat/main.cpp
@@ -58,11 +58,56 @@ template<typename T> T getint() {
 	return val*(neg?-1:1);
 }
 
-struct 
+int n;
+ll val[100001];
+vector<int> adj[100001];
+
+ll dp1[100001];
+ll dp2[100001]; //max már van
+ll dp3[100001]; //min már van
+
+const ll inf=10000000000000000LL;
+
+void dfs(int x) {
+	if(sz(adj[x])==0) {
+		dp1[x]=0;
+		dp2[x]=val[x];
+		dp3[x]=-val[x];
+		return ;
+	}
+	
+	ll szum=0;
+	for(auto i:adj[x]) {
+		dfs(i);
+		szum+=dp1[i];
+	}
+	
+	dp1[x]=szum;
+	dp2[x]=val[x]+szum;
+	dp3[x]=-val[x]+szum;
+	for(auto i:adj[x]) {
+		dp1[x]=max(dp1[x], szum-dp1[i]+dp2[i]-val[x]);
+		dp1[x]=max(dp1[x], szum-dp1[i]+dp3[i]+val[x]);
+		
+		dp2[x]=max(dp2[x], szum-dp1[i]+dp2[i]);
+		dp3[x]=max(dp3[x], szum-dp1[i]+dp3[i]);
+	}
+}
 
 int main() {
 	IO;
+	cin>>n;
+	for(int i=0;i<n;++i) {
+		cin>>val[i+1];
+	}
+	for(int i=0;i<n-1;++i) {
+		int a,b;
+		cin>>a>>b;
+		adj[a].pb(b);
+	}
 
+	dfs(1);
+	cout<<dp1[1]<<"\n";
 	return 0;
 }
 
